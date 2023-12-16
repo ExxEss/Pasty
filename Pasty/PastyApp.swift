ee6f6b61cb5e69bf101450813d75449b0322e8d6
ExxEss <yuguoxie@gmail.com>
2023-12-15 19:47:56 -0600

    Initial Commit

---
+++ Pasty/PastyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PastyApp.swift
+//  Pasty
+//
+//  Created by EssExx on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PastyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
